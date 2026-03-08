706632ff47fb7238413d9fb514da099eef8a5338
Kris Simon <kris@ausdertechnik.de>
2026-03-08 10:49:54 +0100

    Add WhileLoop and BreakStatement visit methods to test visitors
    NodeCounterVisitor and VariableCollectorVisitor in VisitorPatternTests
    were missing the visit methods required by ASTVisitor after WhileLoop
    and BreakStatement were added to the AST (ARO-0131).

--- Tests/AROParserTests/VisitorPatternTests.swift
+++ Tests/AROParserTests/VisitorPatternTests.swift
@@ -137,6 +137,18 @@ struct NodeCounterVisitor: ASTVisitor {
     func visit(_ node: ErrorStatement) throws -> Int {
         1
     }
+
+    func visit(_ node: WhileLoop) throws -> Int {
+        var count = 1
+        for stmt in node.body {
+            count += try stmt.accept(self)
+        }
+        return count
+    }
+
+    func visit(_ node: BreakStatement) throws -> Int {
+        1
+    }
 }
 
 /// Collects all variable base names
@@ -270,6 +282,18 @@ struct VariableCollectorVisitor: ASTVisitor {
     }
 
     func visit(_ node: ErrorStatement) throws -> Set<String> {
+        []
+    }
+
+    func visit(_ node: WhileLoop) throws -> Set<String> {
+        var vars: Set<String> = []
+        for stmt in node.body {
+            vars.formUnion(try stmt.accept(self))
+        }
+        return vars
+    }
+
+    func visit(_ node: BreakStatement) throws -> Set<String> {
         []
     }
 }
