299db35ff1d34a84c4c23c21c8c724a6db090a9d
Kris Simon <kris@ausdertechnik.de>
2026-03-10 08:18:34 +0100

    fix(tests): add RangeLoop visitor implementations to VisitorPatternTests

--- Tests/AROParserTests/VisitorPatternTests.swift
+++ Tests/AROParserTests/VisitorPatternTests.swift
@@ -70,6 +70,14 @@ struct NodeCounterVisitor: ASTVisitor {
         return count
     }
 
+    func visit(_ node: RangeLoop) throws -> Int {
+        var count = 1
+        for stmt in node.body {
+            count += try stmt.accept(self)
+        }
+        return count
+    }
+
     func visit(_ node: PipelineStatement) throws -> Int {
         var count = 1
         for stmt in node.stages {
@@ -195,6 +203,14 @@ struct VariableCollectorVisitor: ASTVisitor {
         if let index = node.indexVariable {
             vars.insert(index)
         }
+        for stmt in node.body {
+            vars.formUnion(try stmt.accept(self))
+        }
+        return vars
+    }
+
+    func visit(_ node: RangeLoop) throws -> Set<String> {
+        var vars: Set<String> = [node.variable]
         for stmt in node.body {
             vars.formUnion(try stmt.accept(self))
         }
